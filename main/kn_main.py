4b3272ff7734559564edbeca538e90c0ce2df379
Krzysztof Nowak <k.nowak@cern.ch>
2018-03-01 21:42:45 +0100

    foobar
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- main/kn_main.py
+++ main/kn_main.py
@@ -34,6 +34,8 @@ score = 0
 rides_idxs = dict((i, r) for i, r in enumerate(rides))
 i=0
 while t < T:
+    if not rides_idxs:
+        break
     for car_idx, (car_r, car_c) in enumerate(car_pos):
         choices = []
 
@@ -42,8 +44,9 @@ while t < T:
 
         for_removal = []
         for ride_idx, ride in rides_idxs.items():
+            ride_r0, ride_c0, ride_r1, ride_c1, t_start, t_end = ride
             #ride = rides[ride_idx]
-            if ride_taken[ride_idx]:
+            if ride_taken[ride_idx] or t_end < t:
                 for_removal.append(ride_idx)
                 continue
             rdist = ride_dists[ride_idx]
@@ -52,9 +55,10 @@ while t < T:
                 choices.append((ride_idx, t_finish, arrive_dist, rdist, bonus))
         # select min t_finish
         if choices:
-            #ride_idx, t_finish, arrive_dist, rdist, bonus = min(choices, key=lambda x: (x[1], (-x[3]-x[4])))
-            #ride_idx, t_finish, arrive_dist, rdist, bonus = min(choices, key=lambda x: x[2])
             ride_idx, t_finish, arrive_dist, rdist, bonus = min(choices, key=lambda x: (x[1], (-x[3]-x[4])))
+            #ride_idx, t_finish, arrive_dist, rdist, bonus = min(choices, key=lambda x: x[2])
+            #ride_idx, t_finish, arrive_dist, rdist, bonus = min(choices, key=lambda x: (x[1], (-x[3]-x[4])))
+            #ride_idx, t_finish, arrive_dist, rdist, bonus = max(choices, key=lambda x: (x[3] + x[4], -x[1]))
             car_choices[car_idx].append(ride_idx)
             ride_taken[ride_idx] = True
             car_free_at[car_idx] = t_finish
@@ -64,6 +68,8 @@ while t < T:
             del rides_idxs[idx]
     i += 1
     if i % 1000 == 0:
+        print(t, T, len(rides_idxs))
+        print("Score: {}".format(score))
         save_data(car_choices, prefix='kn_spam')
     t = max(min(car_free_at), t + 1)
 
