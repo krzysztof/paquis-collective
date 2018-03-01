46c0ccb8e8cc9c61dfaefb3f127a99a094d2abdb
Krzysztof Nowak <k.nowak@cern.ch>
2018-03-01 18:58:27 +0100

    modified template for input reading
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- main/template.py
+++ main/template.py
@@ -7,20 +7,10 @@ with open(infile, 'r') as fp:
     lines = [l.strip() for l in fp.readlines()]
 # Some common follow-ups:
 # Usually first line specifies some numbers A, B, C, D, etc.:
-# A, B, C, D = [int(x) for x in lines[0].split(' ')]
+R, C, F, N, B, T = [int(x) for x in lines[0].split(' ')]
 
 # Reading A lines into tuple of ints
-# AA = list(map(lambda x: tuple(map(int, x.split(' '))), lines[1: A+1]))
-
-# Reading B lines after taht into tuple of ints
-# BB = list(map(lambda x: tuple(map(int, x.split(' '))), lines[A+1: A+B+1]))
-
-# Reading C lines after taht into tuple of ints
-# CC = list(map(lambda x: tuple(map(int, x.split(' '))), lines[A+B+1: A+B+C+1]))
-
-# Reading all input into an array (e.g. A x B)
-# arr = lines[1:]  # The rest of input
-
+rides = list(map(lambda x: tuple(map(int, x.split(' '))), lines[1: ]))
 
 def write_output(data, prefix="", rnd=0):
     outfile = os.path.join('output', prefix, os.path.basename(infile) + '.output')
